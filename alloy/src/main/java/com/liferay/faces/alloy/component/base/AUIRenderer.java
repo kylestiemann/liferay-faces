f610ba362eedaac70742c49ac951c72fd6675a99
kylestiemann <kyle.stiemann@liferay.com>
2014-01-13 15:13:09 -0500

    Re-added AUIRenderer.java

---
+++ alloy/src/main/java/com/liferay/faces/alloy/component/base/AUIRenderer.java
@@ -0,0 +1,16 @@
+package com.liferay.faces.alloy.component.base;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.render.Renderer;
+
+public abstract class AUIRenderer extends Renderer {
+
+	@Override
+	public void encodeBegin(FacesContext facesContext, UIComponent uiComponent) throws IOException {
+		super.encodeBegin(facesContext, uiComponent);
+	}
+	
+}
