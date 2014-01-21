332aaa4141105cd32e8829106fca17b0209c9038
Bruno Basto <bruno.basto@liferay.com>
2014-01-21 11:42:39 -0500

    Adding javascript API to expose component variables

---
+++ util/src/main/resources/META-INF/liferay-faces/liferay-faces.js
@@ -0,0 +1,32 @@
+LF = window.LF || {};
+
+(function(LF) {
+	var components = {};
+	var componentsFn = {};
+
+	LF.component = function(id, value) {
+		var retVal;
+
+		if (arguments.length === 1) {
+			var component = components[id];
+
+			if (component && Lang.isFunction(component)) {
+				componentsFn[id] = component;
+
+				component = component();
+
+				components[id] = component;
+			}
+
+			retVal = component;
+		}
+		else {
+			retVal = (components[id] = value);
+		}
+
+		return retVal;
+	};
+
+	LF._components = components;
+	LF._componentsFn = components;
+})(LF);
